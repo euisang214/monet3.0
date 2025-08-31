e3395225e6efe3a4201bab9044972a7579f653d6
Ethan Shin <euisang214@gmail.com>
2025-08-31 10:00:52 +0900

    deleted prisma migration files

--- prisma/migrations/20250825000000_add_user_names/migration.sql
+++
@@ -1,4 +0,0 @@
--- Add firstName and lastName columns to User table
-ALTER TABLE "User"
-  ADD COLUMN "firstName" TEXT,
-  ADD COLUMN "lastName" TEXT;
--- prisma/migrations/20250826000000_add_stripe_ids/migration.sql
+++
@@ -1,4 +0,0 @@
--- Add Stripe columns to User table
-ALTER TABLE "User"
-  ADD COLUMN "stripeCustomerId" TEXT,
-  ADD COLUMN "stripeAccountId" TEXT;
