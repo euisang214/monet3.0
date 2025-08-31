05ad023ffb307f8b7de97b661b943f91aacce164
Ethan Shin <euisang214@gmail.com>
2025-09-01 08:34:34 +0900

    exposed explicit GET and POST via handler

--- auth.ts
+++ auth.ts
@@ -12,7 +12,7 @@ declare module 'next-auth'{
   }
 }
 
-export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: 'jwt' },
   secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET || 'dev-secret',
   providers: [
