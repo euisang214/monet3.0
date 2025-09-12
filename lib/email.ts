c9f633d812213e2a51af25e6a03a5d40accc43f1
Ethan Shin <euisang214@gmail.com>
2025-09-12 17:31:09 -0400

    removed export of Mail const

--- lib/email.ts
+++ lib/email.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 import type Mail from 'nodemailer/lib/mailer';
 
-export const mailer = nodemailer.createTransport({
+const mailer = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT || 587),
   auth: process.env.SMTP_USER
